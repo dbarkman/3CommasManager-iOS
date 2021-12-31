91d24b7927ab5dfbdf8ac39c5927427cce0a6924
David Barkman <david.barkman13@gmail.com>
2021-12-31 11:58:01 -0700

    Initial Commit

---
+++ 3Commas Manager/_Commas_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Commas_ManagerApp.swift
+//  3Commas Manager
+//
+//  Created by David Barkman on 12/31/21.
+//
+
+import SwiftUI
+
+@main
+struct _Commas_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
